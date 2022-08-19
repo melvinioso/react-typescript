5d455f65df3f441651f0f767b9bc84635779dd55
Matt McGavern <matt.mcgavern@gmail.com>
2022-08-18 22:37:59 -0500

    typescript for refs

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 // import GuestList from './state/GuestList';
 // import UserSearch from './state/UserSearch';
 // import EventComponent from './events/EventComponent';
-import UserSearch from './classes/UserSearch';
+// import UserSearch from './classes/UserSearch';
+import UserSearch from './refs/UserSearch';
 
-const users = [
-  { name: 'Matt', age: 41 },
-  { name: 'Jennifer', age: 43 },
-  { name: 'Dash', age: 5 },
-];
+// const users = [
+//   { name: 'Matt', age: 41 },
+//   { name: 'Jennifer', age: 43 },
+//   { name: 'Dash', age: 5 },
+// ];
 
 const el = document.getElementById('root');
 
@@ -18,7 +19,7 @@ const root = ReactDOM.createRoot(el!);
 const App = () => {
   return (
     <div>
-      <UserSearch users={users}/>
+      <UserSearch />
     </div>
   );
 };
---
+++ src/refs/UserSearch.tsx
@@ -0,0 +1,50 @@
+import { useState, useRef, useEffect } from 'react';
+
+const users = [
+  { name: 'Matt', age: 41 },
+  { name: 'Jennifer', age: 43 },
+  { name: 'Dash', age: 5 },
+];
+
+const UserSearch: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [name, setName] = useState('');
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  useEffect(() => {
+    if (!inputRef.current) {
+      return;
+    }
+
+    inputRef.current.focus();
+  }, []);
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+
+    setUser(foundUser);
+  };
+
+  return (
+    <div>
+      User Search
+      <div>
+        <input
+          ref={inputRef}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button onClick={onClick}>Find User</button>
+        <div>
+          {user && user.name}
+          <br />
+          {user && user.age}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserSearch;
